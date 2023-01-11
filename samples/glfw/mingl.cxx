5da6b2852e8677882c1b7d5eb89e17daf4bd5899
Sonejght <sonejght@gmail.com>
2023-01-11 03:49:11 +0200

    fix

--- samples/glfw/mingl.cxx
+++ samples/glfw/mingl.cxx
@@ -1,5 +1,4 @@
 #define GLFW_INCLUDE_NONE
-//#include <glad/gl.h>
 #include <son8/opengl.hxx>
 #include <GLFW/glfw3.h>
 
@@ -25,8 +24,7 @@ int main()
         if (key ==GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, GLFW_TRUE);
     });
 
-    glfwSwapInterval(1);
-    GLfloat rgb[3] = {0.5, 0.25, 0.0};
+    glfwSwapInterval(0);
 
     while (!glfwWindowShouldClose(window)) {
         auto timesec = glfwGetTime();
@@ -34,7 +32,7 @@ int main()
         auto fading = 1 * slowness;
         assert(glGetError() == GL_NO_ERROR);
         glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(fading + rgb[0], fading + rgb[1], fading + rgb[2], 0.f);
+        glClearColor(fading, fading, fading, 0.f);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
