014aceb230ae3585f1b83af51d436e6969e2fa7b
Sonejght <sonejght@gmail.com>
2023-01-22 10:08:58 +0200

    update coatglop, use coatglop wrapper for samplegl mingl

--- samples/son8samplegl/mingl.cxx
+++ samples/son8samplegl/mingl.cxx
@@ -2,12 +2,15 @@
 #include <son8/opengl-1_1.hxx>
 #include <son8/window.hxx>
 
+namespace gl = son8::opengl;
+namespace GL = son8::opengl::enums;
+
 int main()
 {
     son8::window_init(640, 480);
 
     son8::run([](){
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(0.8, 0.8, 0.2, 1.0);
+        gl::Clear();
+        gl::ClearColor(.8f, .8f, .2f, 1.f);
     });
 }
